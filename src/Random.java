849f19a58975522a05d8045a54805b9732239e03
Elise Glaser <eglaser97@gmail.com>
2018-12-06 11:18:06 -0700

    Revert "Added a print method!"
    This reverts commit c8ac0a59d001e3022c06f3d6b1e882c75638aecd.

--- src/Random.java
+++ src/Random.java
@@ -1,13 +1,4 @@
 
 public class Random {
-	int randomInt;
-	public Random() {
-		
-	}
-	public int giveRandomint() {
-		return 2;
-	}
-	public void printStuff() {
-		System.out.println("hey");
-	}
+
 }
