c8ac0a59d001e3022c06f3d6b1e882c75638aecd
Elise Glaser <eglaser97@gmail.com>
2018-12-06 11:04:13 -0700

    Added a print method!

--- src/Random.java
+++ src/Random.java
@@ -1,4 +1,13 @@
 
 public class Random {
-
+	int randomInt;
+	public Random() {
+		
+	}
+	public int giveRandomint() {
+		return 2;
+	}
+	public void printStuff() {
+		System.out.println("hey");
+	}
 }
