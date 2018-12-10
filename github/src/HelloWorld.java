4dbb9e3691636da6b34d41c60ea28fc51f28cbbb
maggiehorowitz <m_horowitz@coloradocollege.edu>
2018-12-10 10:39:58 -0700

    maggie's commit 2

--- github/src/HelloWorld.java
+++ github/src/HelloWorld.java
@@ -1,8 +1,10 @@
 
 public class HelloWorld {
 	System.out.println("Hello World");
-	System.out.println("Teammate 1, You're the best! Good job setting up Git Repository!!!");
+	System.out.println("Teammate 1, You're the best! Good job setting up Git Repository!!! - ");
 	System.out.println("Hi Hi Hi");
 	
 	System.out.println("Ian is the best!!!")
+	
+	System.out.println("NO MAGGIE IS THE BEST. - maggie's commit #2");
 }
