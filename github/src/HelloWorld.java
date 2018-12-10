f8e8a7244a2e02d58d6ffb1bcbd3e52da8143426
maggiehorowitz <m_horowitz@coloradocollege.edu>
2018-12-10 10:33:48 -0700

    welcome messages teammate2

--- github/src/HelloWorld.java
+++ github/src/HelloWorld.java
@@ -1,6 +1,8 @@
 
 public class HelloWorld {
-
 	System.out.println("Hello World");
 	System.out.println("Teammate 1, You're the best! Good job setting up Git Repository!!!");
+	System.out.println("Hi Hi Hi");
+	
+	
 }
